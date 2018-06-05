272b075fe9e67475b87f87a7c6c117e7b91b082b
zxc854560673 <854560673@qq.com>
2018-06-05 18:23:43 +0800

    fix a bug
    for mysql connect.init

--- ftp-client/main.py
+++ ftp-client/main.py
@@ -8,6 +8,6 @@ for root, dirs, files in os.walk('plugins'):
     for i in files:
         if not i=='__init__.py':__import__('plugins.'+i.split('.')[0])
 configloader.init()
-#connect_mysql.init()
+connect_mysql.init()
 loop=asyncio.get_event_loop()
 loop.run_until_complete(asyncio.wait([connect.connect()]))
