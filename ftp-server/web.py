fd438db1af178493e810f871521e572bd1dda9d6
zxc854560673 <854560673@qq.com>
2018-06-11 23:38:41 +0800

    set port by config, default is 8080

--- ftp-server/web.py
+++ ftp-server/web.py
@@ -1,8 +1,9 @@
 #This is the main file to run
 import asyncio
 import gb
+import configloader as co
 gb.init()
 import urls
 from aiohttp import web
 def server_start():
-    web.run_app(urls.init())
+    web.run_app(urls.init(),port=co.config['port'] if 'port' in co.config else 8080)
